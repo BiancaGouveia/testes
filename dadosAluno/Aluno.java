56287e8552531193a59510915791772a5fb130f2
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-03-27 01:20:47 +0000

    criei a classe aluno com get e set

---
+++ dadosAluno/Aluno.java
@@ -0,0 +1,11 @@
+class Aluno {
+    private String nome
+
+    public void setNome(String novoNome) { // definir o nome
+        nome = novoNome;
+    }
+
+    public String getNome() { // obter o nome
+        return nome;
+    }
+}
