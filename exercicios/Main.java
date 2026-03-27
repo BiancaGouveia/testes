9050c75fa3fbe573171157c769004587168ca221
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-03-27 04:04:31 +0000

    ajustando os devidos codigos com o modificador de acesso protected

--- exercicios/Main.java
+++ exercicios/Main.java
@@ -3,13 +3,5 @@ import exercicios.Produto;
 
 public class Main extends Produto {
     public static void main(String[] args) {
-        Produto prod = new Produto();
-            prod.setNome("pringles");
-            prod.setPreco(12.50);
-            prod.setQuantidadeEmEstoque(105);
-
-            System.out.println("Produto: " + prod.getNome());
-            System.out.println("valor do produto: R$" + prod.getPreco());
-            System.out.println("quantidade em estoque: " + prod.getQuantidadeEmEstoque());
-    }
+       
 }
--- exercicios/Produto.java
+++ exercicios/Produto.java
@@ -1,31 +1,9 @@
 package exercicios;
 
 class Produto {
-    private String nome;
-    private double preco;
-    private int quantidadeEmEstoque;
+    protected String nome;
+    protected double preco;
+    protected int quantidadeEmEstoque;
 
-    public void setNome(String nomeInit) { // definindo o nome
-        nome = nomeInit;
-    }
-
-    public void setPreco(double precoInit) { // definindo o preco
-        preco = precoInit;
-    }
-
-    public void setQuantidadeEmEstoque(int quantidadeEmEstoqueInit) { // definindo a qtd em estoque
-        quantidadeEmEstoque = quantidadeEmEstoqueInit;
-    }
-
-    public String getNome() { // pegando o nome
-        return nome;
-    }
-
-    public double getPreco() { // pegando o preco
-        return preco;
-    }
-
-    public int getQuantidadeEmEstoque() { // pegando a quantidade em estoque
-        return quantidadeEmEstoque;
-    }
+    
 }
