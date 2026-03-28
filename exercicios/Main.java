d2c69e7f6f31dfb6e3ace0e0a16d68ea1fefa299
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-03-28 01:11:27 +0000

    deletei o arquivo 'ProdutoPremium.java'

--- exercicios/Main.java
+++
@@ -1,7 +0,0 @@
-package exercicios;
-import exercicios.Produto;
-
-public class Main extends Produto {
-    public static void main(String[] args) {
-       
-}
--- exercicios/Produto.java
+++ exercicios/Produto.java
@@ -1,9 +1,19 @@
 package exercicios;
 
 class Produto {
-    protected String nome;
-    protected double preco;
-    protected int quantidadeEmEstoque;
+    private String nome;
+    private double preco;
+    private int qtdEstoque;
 
-    
+    public void setNome(String nomeInit) { // definindo nome
+        nome = nomeInit;
+    }
+
+    public void setPreco(double precoInit) { // definindo preco
+        preco = precoInit;
+    }
+
+    public void int setQtdEstoque(int qtdEstoqueInit) { // definindo estoque
+        
+    }
 }
