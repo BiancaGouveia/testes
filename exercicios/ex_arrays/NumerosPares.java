ddadddc5430972f755d4975359b88c584866e2c7
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-04-14 21:13:50 +0000

    chore: organiza arquivos na pasta correta

---
+++ exercicios/ex_arrays/NumerosPares.java
@@ -0,0 +1,16 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        int[] arrayPares = new int[6];
+        
+        arrayPares[0] = 2;
+        arrayPares[1] = 4;
+        arrayPares[2] = 6;
+        arrayPares[3] = 8;
+        arrayPares[4] = 10;
+        arrayPares[5] = 12;
+
+        for(int i=0; i<arrayPares.length; i++) {
+            System.out.println(arrayPares[i]);
+        }
+    }
+}
