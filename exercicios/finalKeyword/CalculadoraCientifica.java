fb4aaa81d1e890b8d3987b9f52dcbfe9a358d5f2
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-04-20 19:23:17 +0000

    feat: criando novo arquivo .java

---
+++ exercicios/finalKeyword/CalculadoraCientifica.java
@@ -0,0 +1,19 @@
+class Calculadora {
+    final somar(int a, int b) {
+
+    }
+}
+
+public class CalculadoraCientifica {
+    public static void main(String[] args) {
+        Calculadora info = new Calculadora();
+
+        info.somar(1, 2);
+        
+        /*
+        como comentado no próprio exercicio da udemy, 
+        essa declaracao dos valores que estão dentro
+        do método final vai dar erro.
+         */
+    }
+}
