add17ebbd9e37438ba677443c0c6e63af38cbe0f
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-05-02 01:09:51 +0000

    chore: remove arquivos.class

--- exercicios/generics/Caixa.java
+++ exercicios/generics/Caixa.java
@@ -12,6 +12,12 @@ public class Caixa<T> {
 
     public static void main(String[] args) {
         // Criando uma caixa para guardar Strings
+
+       /*
+       Caixa<String> → é o tipo da referência (classe genérica com String)
+       new Caixa<>() → cria um objeto na memória
+       caixaDeTexto → é a variável que aponta para esse objeto
+       */
         Caixa<String> caixaDeTexto = new Caixa<>();
         
         caixaDeTexto.guardar("Oi, mundo!");
