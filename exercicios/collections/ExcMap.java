a393668ad5542e776e468aad53c245630732e792
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-05-06 00:32:04 +0000

    feat: cria e implementa arquivo 'ExcMap.java' com o intuito em entender e aplicar conceitos de coleção em Java, especificamente o uso de Map para gerenciar um estoque de produtos

---
+++ exercicios/collections/ExcMap.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class ExcMap {
+    public static void main(String[] args) {
+        Map<String, Integer> estoque = new HashMap<>();
+
+        estoque.put("P001", 10);
+        estoque.put("P002", 11);
+        estoque.put("P003", 12);
+        estoque.put("P004", 13);
+
+        // lembrando que não é ordenado
+        System.out.println("Estoque inicial: "+ estoque);
+
+        // verificando quantidade disponível de um produto específico
+        System.out.println("quantidade disponpivel do produto 'P003': " + estoque.get("P003"));
+
+        // removendo uma quantidade especifica de produto do estoque
+        estoque.remove("P002");
+
+        // atualizando o estoque (lembrando que não é ordenado)
+        System.out.println("estoque após remoção: " + estoque);
+    }
+}
