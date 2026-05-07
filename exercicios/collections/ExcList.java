185a2c6654cbbeff7697f6d38e8d7d92fd9d7b37
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-05-07 01:19:22 +0000

    feat: implementa no arquivo 'ExcList.java' o objetivo de entender e aplicar conceitos de listas em java, utilizando a classe ArrayList para gerenciar uma lista de filmes.

--- exercicios/collections/ExcList.java
+++ exercicios/collections/ExcList.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ExcList {
     public static void main(String[] args) {
@@ -16,6 +17,25 @@ public class ExcList {
         // verificando se existe na lista o filme "coraline" (volta true)
         System.out.println("O filme 'Coraline' está na lista? "+ lista.contains("Coraline"));
 
+        List<String> lista2 = Arrays.asList(
+            "Alice no País das Maravilhas",
+            "Peter Pan"
+        );
         
+        // adicionando conteúdos da lista2 na lista
+        lista.addAll(lista2);
+
+        // como a lista fica depois de adicionar 
+        System.out.println("lista com lista2: " + lista);
+
+        // removendo HarryPotter da lista
+        lista.remove("Harry Potter");
+        System.out.println("O filme 'Harry Potter' ainda está na lista?: " + lista.contains("Harry Potter"));
+
+        System.out.println("lista atualizada: " + lista);
+
+        // lista depois de limpa
+        lista.clear();
+        System.out.println("lista limpa: " + lista);
     }
 }
