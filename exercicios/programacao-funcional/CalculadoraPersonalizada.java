2209aa128e94f0170e3f995da06e763570a715bb
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-05-18 00:24:58 +0000

    feat: implementa dentro do arquivo 'CalculadoraPersonalizada.java' uma calculadora funcional que executa operações matemáticas específicas

--- exercicios/programacao-funcional/CalculadoraPersonalizada.java
+++ exercicios/programacao-funcional/CalculadoraPersonalizada.java
@@ -1,4 +1,17 @@
 @FunctionalInterface
 interface OperacaoMatematica {
+
     double executar(double a, double b); // metodo abstrato
+
+}
+
+public class CalculadoraPersonalizada {
+    public static void main(String[] args) {
+
+       OperacaoMatematica potencia = (a, b) -> Math.pow(a, b);
+       OperacaoMatematica raizQuadrada = (a, b) -> Math.sqrt(a);
+
+        System.out.printf("Potênciação: %.4f\n" , potencia.executar(2, 10));
+        System.out.printf("Raiz quadrada: %.4f\n" , raizQuadrada.executar(15, 0));
+    }
 }
